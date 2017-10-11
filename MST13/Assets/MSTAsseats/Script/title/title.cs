6a372f0230eb2509d4afffed680b967075f94a42
関谷富明 <sekiyatomiakira@sekiyatomimeinoMacBook-Pro.local>
2017-10-11 10:15:49 +0900

    title画像を更新

---
+++ MST13/Assets/MSTAsseats/Script/title/title.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 作成者　：　関谷富明
+/// 更新日　：
+/// 内容　　；　titlesceneの制御
+/// </summary>
+public class title : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
