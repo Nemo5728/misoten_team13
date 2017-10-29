d60aa816288e9a602f27960b50e0d247fdc946fd
関谷富明 <sekiyatomiakira@shiteibouryokudanHALtoukyou.local>
2017-10-30 00:05:32 +0900

    ：DemoSceneを追加　--- モーションやエフェクトなどの実験用 ：characterのアニメーターの実装：（修正予定） ：ラブゲージ表示UIの追加

---
+++ MST13/Assets/MSTAsseats/Script/Demo/dogMotionDemo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dogMotionDemo : MonoBehaviour {
+
+	private Animator anime;  		// アニメーション(ミニオン（犬））
+
+	// Use this for initialization
+	void Start ()
+	{
+		anime = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// キーを推してモーションテスト！
+		// 上キー押したら
+		if (Input.GetKey ("up")) 
+		{
+			// 移動モーション
+			anime.SetBool ("dogMove", true);
+		} 
+		// 上キーを話したら
+		else if (Input.GetKeyUp ("up"))
+		{
+			// 待機状態へ
+			anime.SetBool ("dogMove",false );
+		}
+
+		if (Input.GetKeyDown ("e")) 
+		{
+			// ダウンモーションへ
+			anime.SetTrigger("dogDown");
+		}
+
+
+		if (Input.GetKeyDown ("r")) 
+		{
+			// 召喚モーションへ
+			anime.SetTrigger("dogSpawn");
+		}
+
+		if (Input.GetKeyDown ("a"))
+		{
+			// 弱攻撃モーションへ
+			anime.SetTrigger("dogAttack_A");
+		}
+		else if (Input.GetKeyDown ("s"))
+		{
+			// 強攻撃モーションへ
+			anime.SetTrigger("dogAttack_B");
+		}
+
+		if (Input.GetKeyDown ("b")) 
+		{
+			// ダメージモーションへ
+			anime.SetTrigger("dogDamage");
+		}
+	}
+}
---
+++ MST13/Assets/MSTAsseats/Script/Demo/monsterMotionDemo.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monsterMotionDemo : MonoBehaviour {
+
+	private Animator anime;  		// アニメーション(ラブモンスター)
+
+	// Use this for initialization
+	void Start ()
+	{
+		anime = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// キーを推してモーションテスト！
+		// 上キー押したら
+		if (Input.GetKey ("up")) 
+		{
+			// 移動モーション
+			anime.SetBool ("monsterMove", true);
+		} 
+		// 上キーを話したら
+		else if (Input.GetKeyUp ("up"))
+		{
+			// 待機状態へ
+			anime.SetBool ("monsterMove",false );
+		}
+
+		if (Input.GetKeyDown ("e")) 
+		{
+			// ダウンモーションへ
+			anime.SetTrigger("monsterDown");
+		}
+
+		if (Input.GetKeyDown ("r")) 
+		{
+			// 召喚モーションへ
+			anime.SetTrigger("monsterTransform");
+		}
+
+		if (Input.GetKeyDown ("a"))
+		{
+			// 弱攻撃モーションへ
+			anime.SetTrigger("monsterAttack_A");
+		}
+		else if (Input.GetKeyDown ("s"))
+		{
+			// 強攻撃モーションへ
+			anime.SetTrigger("monsterAttack_B");
+		}
+
+		if (Input.GetKeyDown ("b")) 
+		{
+			// ダメージモーションへ
+			anime.SetTrigger("monsterDamage");
+		}
+
+		if (Input.GetKeyDown ("v")) 
+		{
+			// 消滅モーションへ
+			anime.SetTrigger("monsterSplit");
+		}
+
+	}
+}
