0cf77db1a86737ca35bf1e1c45f03ac8cf528d36
関谷富明 <sekiyatomiakira@shiteibouryokudanHALtoukyou.local>
2017-11-01 11:00:04 +0900

    近藤くん作成のresultを追加

---
+++ MST13/Assets/MSTAsseats/Script/Demo/monster.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TrueSync;
+
+public class monster : TrueSyncBehaviour {
+
+	private const byte INPUT_KEY_FORWARD = 0;	// 前移動
+	private const byte INPUT_KEY_BACK = 1;		// 後移動
+	private const byte INPUT_KEY_RIGHT = 2;     // 右移動
+	private const byte INPUT_KEY_LEFT = 3;      // 左移動
+	private const byte INPUT_KEY_ATTACK_A = 4;	// 弱攻撃
+	private const byte INPUT_KEY_ATTACK_B = 5;	// 強攻撃
+	private const byte INPUT_KEY_BUTTON = 6;	// ボタン
+
+	private TSRigidBody rb = null;		// リジット
+	private Animator anime;				//アニメーター
+	private TSVector directionVector = TSVector.zero;
+
+	public float 	speed;					// 速度
+	public int   	attackPower;			// 攻撃力
+
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	// 初期化
+	public override void OnSyncedStart()
+	{
+		anime = GetComponent<Animator> ();	//アニメーションの取得
+		rb = GetComponent<TSRigidBody>();	// RigidBodyの取得
+	}
+
+	// インプット
+	public override void OnSyncedInput()
+	{
+		bool forward = Input.GetKey(KeyCode.W);
+		bool back = Input.GetKey(KeyCode.S);
+		bool right = Input.GetKey(KeyCode.D);
+		bool left = Input.GetKey(KeyCode.A);
+		bool attackA = Input.GetKeyDown (KeyCode.G);
+		bool attackB = Input.GetKeyDown (KeyCode.H);
+
+		TrueSyncInput.SetBool(INPUT_KEY_FORWARD, forward);
+		TrueSyncInput.SetBool(INPUT_KEY_BACK, back);
+		TrueSyncInput.SetBool(INPUT_KEY_RIGHT, right);
+		TrueSyncInput.SetBool(INPUT_KEY_LEFT, left);
+		TrueSyncInput.SetBool (INPUT_KEY_ATTACK_A, attackA);
+		TrueSyncInput.SetBool (INPUT_KEY_ATTACK_B, attackB);
+	}
+
+	// 更新
+	public override void OnSyncedUpdate()
+	{
+		bool forward = TrueSyncInput.GetBool(INPUT_KEY_FORWARD);
+		bool back = TrueSyncInput.GetBool(INPUT_KEY_BACK);
+		bool right = TrueSyncInput.GetBool(INPUT_KEY_RIGHT);
+		bool left = TrueSyncInput.GetBool(INPUT_KEY_LEFT);
+		bool attackA = TrueSyncInput.GetBool (INPUT_KEY_ATTACK_A);
+		bool attackB = TrueSyncInput.GetBool (INPUT_KEY_ATTACK_B);
+
+		TSVector vector = TSVector.zero;
+		if (forward)
+		{
+			directionVector = vector += TSVector.forward;
+			// 移動モーション
+			anime.SetBool ("monsterMove", true);
+		}
+		if (back)
+		{
+			directionVector = vector += TSVector.back;
+		}
+		if (left)
+		{
+			directionVector = vector += TSVector.left;
+		}
+		if (right)
+		{
+			directionVector = vector += TSVector.right;
+		}
+
+		// 攻撃中は移動不可
+		if( attackA)
+		{
+			// 弱攻撃モーションへ
+			anime.SetTrigger("monsterAttack_A");
+		}
+		TSVector.Normalize(vector);
+
+		// addForceで移動量を加算
+	//	rb.AddForce(speed * vector, ForceMode.Force);
+		rb.position += speed * vector;
+		Debug.Log (speed);
+
+		// 角度を設定する
+		//FP direction = TSMath.Atan2(directionVector.x, directionVector.z) * TSMath.Rad2Deg;
+		//transform.rotation = Quaternion.Euler(0.0f, (float)direction, 0.0f);
+	}
+}
---
+++ MST13/Assets/MSTAsseats/Script/result/ranking.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TrueSync;
+
+public class ranking : MonoBehaviour {
+
+    public int[] score;
+    public GameObject players;
+    public GameObject texts;
+
+    private int[] pr_score = new int[4];
+    private int[] pr_rank = new int[4];
+
+    private float y = 0.0f;
+    private int a = 0;
+
+    // Use this for initialization
+    void Start () {
+        TrueSyncManager.EndSimulation();
+
+        score.CopyTo(pr_score, 0);
+
+        System.Array.Sort(pr_score);
+
+        //ランキングソート
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (pr_score[i] == score[j])
+                {
+                    pr_rank[j] = 4 - i;
+                    break;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < players.transform.childCount; i++)
+        {
+            Transform text;
+
+            //プレイヤー達の高さの設定
+            y = ( 4 - pr_rank[i] ) * 0.75f;
+            players.transform.GetChild(i).transform.Translate ( 0.0f, y, 0.0f );
+
+            //ランキングのテキスト表記
+            text = texts.transform.GetChild(i);
+            text.transform.Translate(0.0f, (y+2.5f), 0.0f);
+            text.GetComponent<TextMesh>().text = "PC" + (i + 1) + ":" + pr_rank[i].ToString() + "位";
+            text.transform.localScale = new Vector3 ( -1, 1, 1 );
+
+            //もし1＝一位なら
+            if (pr_rank[i] == 1)
+            {
+                //一位固有演出をするプレイヤーの設定
+                a = i;
+
+                text.GetComponent<TextMesh>().fontSize = 30;
+            }
+            else
+            {
+                text.GetComponent<TextMesh>().fontSize = 20;
+                text.GetComponent<TextMesh>().color *= new Color ( 0.5f, 0.5f, 0.5f, 0.8f );
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        players.transform.GetChild(a).transform.Rotate( 0.0f, 0.5f, 0.0f );
+        for (int i = 0; i < 4; i++)
+        {
+            texts.transform.GetChild(i).transform.LookAt( Camera.main.transform );
+        }
+    }
+}
