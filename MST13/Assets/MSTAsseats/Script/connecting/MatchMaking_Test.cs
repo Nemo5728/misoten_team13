28fb683758cd568c745eb139ca2318281807f3bd
関谷富明 <sekiyatomiakira@shiteibouryokudanHALtoukyou.local>
2017-11-02 16:59:03 +0900

    コネクトシーンのverUP 。。。サーバーに接続されてないと接続失敗のテキスト描画　→　Titleシーンへ遷移

--- MST13/Assets/MSTAsseats/Script/connecting/MatchMaking_Test.cs
+++ MST13/Assets/MSTAsseats/Script/connecting/MatchMaking_Test.cs
@@ -1,13 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MatchMaking_Test : Photon.PunBehaviour {
- 
+	
+	private bool serverStatus;
+	private Text startText;
+	private Text failedText;
+	private float startAlpha;
+	private float failedAlpha;
+	
     void Start () {
-        PhotonNetwork.ConnectUsingSettings ("v1.0");
+		startText = GameObject.Find("Canvas/Start").GetComponent<Text>();
+		failedText = GameObject.Find("Canvas/Failed").GetComponent<Text>();
+		
+		PhotonNetwork.ConnectUsingSettings("v1.0");
         PhotonNetwork.automaticallySyncScene = true;
     }
+	
+//	public void OnConnectionFail() {
+//		Debug.Log( "接続エラー：　接続失敗." );
+//	}
+	
+	public void OnFailedToConnectToPhoton() {
+		Debug.Log( "サーバーエラー：　接続失敗." );
+		
+		startAlpha = Mathf.Clamp(0.0f, 0, 1.0f);
+		failedAlpha = Mathf.Clamp(1.0f, 0, 1.0f);
+		
+		Color sa = startText.color;
+		Color fa = failedText.color;
+		
+		sa.a = startAlpha;
+		fa.a = failedAlpha;
+		
+		startText.color = sa;
+		failedText.color = fa;
+	}
 	
 	public override void OnJoinedLobby() {
 		PhotonNetwork.JoinOrCreateRoom ("room1", null, null);
@@ -16,12 +46,11 @@ public class MatchMaking_Test : Photon.PunBehaviour {
 	void OnGUI() {
     GUI.Label (new Rect(10, 10, 100, 30), "players: " + PhotonNetwork.playerList.Length);
  
-    if (PhotonNetwork.isMasterClient && GUI.Button (new Rect (10, 40, 100, 30), "start")) {
+    //if (PhotonNetwork.isMasterClient && GUI.Button (new Rect (10, 40, 100, 30), "start")) {
+	if ( GUI.Button (new Rect (10, 40, 100, 30), "start")) {
         //PhotonNetwork.LoadLevel ("Login");
 		
 		SceneManager.LoadScene("Game");
     }
-//	SceneManager.LoadScene("Login");
-	
 }
 }
