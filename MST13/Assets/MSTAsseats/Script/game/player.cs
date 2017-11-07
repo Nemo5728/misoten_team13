69b3a1ae30229a4905690a3582d89c4f3cf54904
nakayamakazuma <gumi.green.m793s@gmail.com>
2017-11-07 23:11:16 +0900

    コントローラー対応 SKY○ね

--- MST13/Assets/MSTAsseats/Script/game/player.cs
+++ MST13/Assets/MSTAsseats/Script/game/player.cs
@@ -9,10 +9,15 @@ public class player : TrueSyncBehaviour {
     private const byte INPUT_KEY_BACK = 1;
     private const byte INPUT_KEY_RIGHT = 2;
     private const byte INPUT_KEY_LEFT = 3;
+    private const byte INPUT_KEY_SPACE = 4;
+    private const byte INPUT_CONTROLLER_STICKX = 5;
+    private const byte INPUT_CONTROLLER_STICKY = 6;
+    private const byte INPUT_CONTROLLER_BUTTON = 7;
+    private const byte INPUT_CONTROLLER_STICKBUTTON = 8;
 
     private TSRigidBody rb = null;
     private TSVector directionVector = TSVector.zero;
-    private ControllerInfo info;
+    private ControllerInfo info = null;
     private float[] minionRespawnCount = new float[15];
     private float playerRespawnCount = 0.0f;
     private int minionCount = 0;
@@ -21,6 +26,10 @@ public class player : TrueSyncBehaviour {
     private float timeLeft = 1.0f;
     private bool transformFlag = false;
     private float transformCount = 0.0f;
+    private int powerUpButton = 0;
+    private float powerUpCount = 0.0f;
+    private bool powerUpFlag;
+    private bool controllerConnect;
 
     [SerializeField, TooltipAttribute("攻撃速度(sec)")] private int attackSpeed = 0;
     [SerializeField, TooltipAttribute("復帰時間(sec)")] private float respawnTime = 0;
@@ -32,16 +41,18 @@ public class player : TrueSyncBehaviour {
     [SerializeField, TooltipAttribute("変身時HP")] private int health = 100;
     [SerializeField, TooltipAttribute("変身時間")] private float transformTime = 100;
     [SerializeField, TooltipAttribute("変身時攻撃力")] private int attack = 1;
+    [SerializeField, TooltipAttribute("連打判定終了時間")] private float powerUpEndTime = 0.5f;
+    [SerializeField, TooltipAttribute("パワーアップ開始回数")] private int powerUpStart = 10;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+    
+    // Update is called once per frame
+    void Update () {
         
-	}
+    }
 
     public override void OnSyncedStart(){
         for (int i = 0; i < markerList.Length; i ++){
@@ -57,6 +68,10 @@ public class player : TrueSyncBehaviour {
 
         knockout = false;
         transformFlag = false;
+        powerUpCount = 0.0f;
+        powerUpButton = 0;
+        powerUpFlag = false;
+        controllerConnect = false;
         rb = GetComponent<TSRigidBody>();
     }
 
@@ -65,14 +80,30 @@ public class player : TrueSyncBehaviour {
         bool back = Input.GetKey(KeyCode.S);
         bool right = Input.GetKey(KeyCode.D);
         bool left = Input.GetKey(KeyCode.A);
+        bool space = Input.GetKeyDown(KeyCode.Space);
 
         TrueSyncInput.SetBool(INPUT_KEY_FORWARD, forward);
         TrueSyncInput.SetBool(INPUT_KEY_BACK, back);
         TrueSyncInput.SetBool(INPUT_KEY_RIGHT, right);
         TrueSyncInput.SetBool(INPUT_KEY_LEFT, left);
+        TrueSyncInput.SetBool(INPUT_KEY_SPACE, space);
 
         //BLEなんちゃら
         info = BLEControlManager.GetControllerInfo();
+
+        if (info != null) controllerConnect = true;
+
+        if(controllerConnect){
+            int stickX = info.stickX;
+            int stickY = info.stickY;
+            bool button = info.isButtonDown;
+            bool stickBtn = info.isStickDown;
+
+            TrueSyncInput.SetInt(INPUT_CONTROLLER_STICKX, stickX);
+            TrueSyncInput.SetInt(INPUT_CONTROLLER_STICKY, stickY);
+            TrueSyncInput.SetBool(INPUT_CONTROLLER_BUTTON, button);
+            TrueSyncInput.SetBool(INPUT_CONTROLLER_STICKBUTTON, stickBtn);
+        }
     }
 
     public override void OnSyncedUpdate(){
@@ -81,26 +112,47 @@ public class player : TrueSyncBehaviour {
             bool back = TrueSyncInput.GetBool(INPUT_KEY_BACK);
             bool right = TrueSyncInput.GetBool(INPUT_KEY_RIGHT);
             bool left = TrueSyncInput.GetBool(INPUT_KEY_LEFT);
+            bool space = TrueSyncInput.GetBool(INPUT_KEY_SPACE);
 
             TSVector vector = TSVector.zero;
-            if (forward)
-            {
-                directionVector = vector += TSVector.forward;
+            if (forward) directionVector = vector += TSVector.forward;
+            if (back) directionVector = vector += TSVector.back;
+            if (left) directionVector = vector += TSVector.left;
+            if (right) directionVector = vector += TSVector.right;
+
+            if(controllerConnect){
+                int stickX = -550 + TrueSyncInput.GetInt(INPUT_CONTROLLER_STICKX);
+                int stickY = -550 + TrueSyncInput.GetInt(INPUT_CONTROLLER_STICKY);
+                bool button = TrueSyncInput.GetBool(INPUT_CONTROLLER_BUTTON);
+                bool stickBtn = TrueSyncInput.GetBool(INPUT_CONTROLLER_STICKBUTTON);
+
+                FP slopeX = stickX / 473;
+                FP slopeY = stickY / 473;
+                directionVector.x = vector.x = speed * slopeX;
+                directionVector.y = vector.y = speed * slopeY;
             }
-            if (back)
-            {
-                directionVector = vector += TSVector.back;
+
+            if(space){
+                powerUpButton++;
+                powerUpCount = 0.0f;
+            }else{
+                powerUpCount += Time.deltaTime;
+
+                if(powerUpCount >= powerUpEndTime){
+                    powerUpCount = 0.0f;
+                    powerUpButton = 0;
+                }
             }
-            if (left)
-            {
-                directionVector = vector += TSVector.left;
-            }
-            if (right)
-            {
-                directionVector = vector += TSVector.right;
+
+            if (powerUpButton > powerUpStart){
+                powerUpFlag = true;
+            }else{
+                powerUpFlag = false;
             }
 
             TSVector.Normalize(vector);
+            TSVector.Normalize(directionVector);
+
             rb.AddForce(speed * vector, ForceMode.Force);
 
             FP direction = TSMath.Atan2(directionVector.x, directionVector.z) * TSMath.Rad2Deg;
@@ -190,4 +242,6 @@ public class player : TrueSyncBehaviour {
             playerRespawnCount = respawnTime;
         }
     }
+
+
 }
