08dea41ee301b0468c9a5389339acd8ba4f41712
関谷富明 <sekiyatomiakira@shiteibouryokudanHALtoukyou.local>
2017-11-21 18:19:19 +0900

    no message

--- MST13/Assets/MSTAsseats/Script/game/readyTime.cs
+++ MST13/Assets/MSTAsseats/Script/game/readyTime.cs
@@ -2,44 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TrueSync;
 
-public class readyTime : Photon.PunBehaviour {
+public class readyTime : TrueSyncBehaviour {
 
     public int MAX_TIME = 4; // カウントダウンの開始値
+    [AddTracking]
     private float timeCounter = 4;
 
 	// Use this for initialization
-	void Start () 
+	void Start () {}
+
+    void Update() {}
+	
+    public override void OnSyncedStart()
     {
         GetComponent<UnityEngine.UI.Text>().text = MAX_TIME.ToString();
-	}
-	
+    }
+
 	// Update is called once per frame
-	void Update () 
+    public override void OnSyncedUpdate()
     {
+        timeCounter -= Time.deltaTime;
 
-            if (PhotonNetwork.isMasterClient)
-            {
-                timeCounter -= Time.deltaTime;
+        if (timeCounter <= -1f)
+        {
+            // Canvasごと削除する
+            // Destroy(gameObject);
+           // TrueSyncManager.Destroy(transform.parent.gameObject);
+            //TrueSyncManager.SyncedDestroy(transform.parent.gameObject);
+            GetComponent<UnityEngine.UI.Text>().text = " ";
 
-                if (timeCounter <= -1f)
-                {
-                    // Canvasごと削除する
-                    Destroy(transform.parent.gameObject);
-                }
-                // マイナス値にならないようにしている
-                else if (timeCounter <= 1f)
-                {
-                    // ゲームスタートを描画
-                    GetComponent<UnityEngine.UI.Text>().text = "GameStart!";
-                }
-                else
-                {
-                    GetComponent<UnityEngine.UI.Text>().text = ((int)timeCounter).ToString();
-                }
+        }
 
-            }
 
+        // マイナス値にならないようにしている
+        if (timeCounter <= 1f)
+        {
+            // ゲームスタートを描画
+            GetComponent<UnityEngine.UI.Text>().text = "GameStart!";
+        }
+
+        else
+        {
+            GetComponent<UnityEngine.UI.Text>().text = ((int)timeCounter).ToString();
+        }
+
+           
+
+/*
         timeCounter -= Time.deltaTime;
 
         if (timeCounter <= -1f)
@@ -57,6 +68,7 @@ public class readyTime : Photon.PunBehaviour {
         {
             GetComponent<UnityEngine.UI.Text>().text = ((int)timeCounter).ToString();
         }
+*/
 
 	}
 }
