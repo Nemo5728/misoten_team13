44279542b448776adef4a1658bd6d10c91d4772c
関谷富明 <sekiyatomiakira@shiteibouryokudanHALtoukyou.local>
2017-12-18 16:29:51 +0900

    minion修正版。

--- MST13/Assets/MSTAsseats/Script/Main/MainManager.cs
+++ MST13/Assets/MSTAsseats/Script/Main/MainManager.cs
@@ -26,6 +26,11 @@ public class MainManager : MonoBehaviour {
     private STATE state;    // 状態遷移
     GameObject selectObj;   // 選択中のオブジェクト
 
+    void Awake()
+    {
+        Application.targetFrameRate = 60; //60FPSに設定
+    }
+
 	// Use this for initialization
 	void Start () 
     {
@@ -38,6 +43,7 @@ public class MainManager : MonoBehaviour {
 
         if (info != null) controllerConnect = true;
 
+        BgmManager.Instance.Play("select");
         state = STATE.STATE_TITLE;
         SetState();
 	}
@@ -127,17 +133,24 @@ public class MainManager : MonoBehaviour {
         {
             case STATE.STATE_TITLE:
                 {
+                    BgmManager.Instance.Stop();
+                    BgmManager.Instance.Play("select");
                     state = STATE.STATE_CONECTING;
+
                     break;
                 }
             case STATE.STATE_CONECTING:
                 {
+                    BgmManager.Instance.Stop();
+                    BgmManager.Instance.Play("select");
                     state = STATE.STATE_LOGIN;
                     break;
                 }
             case STATE.STATE_LOGIN:
                 {
                     LoginCloneDelete();
+                    BgmManager.Instance.Stop();
+                    BgmManager.Instance.Play("gameBGM1");
                     state = STATE.STATE_GAME;
                     break;
                 }
@@ -145,12 +158,16 @@ public class MainManager : MonoBehaviour {
                 {
                     // クローンの削除
                     GameCloneDelete();
+                    BgmManager.Instance.Stop();
+                    BgmManager.Instance.Play("result");
                     state = STATE.STATE_RESULT;
                     break;
                 }
             case STATE.STATE_RESULT:
                 {
                     ResultCloneDelete();
+                    BgmManager.Instance.Stop();
+                    BgmManager.Instance.Play("select");
                     state = STATE.STATE_TITLE;
                     break;
                 }
--- MST13/Assets/MSTAsseats/Script/Main/TitleManager.cs
+++ MST13/Assets/MSTAsseats/Script/Main/TitleManager.cs
@@ -6,22 +6,87 @@ public class TitleManager : MonoBehaviour {
 
     public GameObject logo;
     GameObject obj;
+    float alpha;
 
+    private enum STATE
+    {
+        STATE_START,
+        STATE_NORMAL,
+        STATE_END
+    };
+
+    STATE state;
 	// Use this for initialization
 	void Start ()
     {
-       
+        alpha = 0f;
         obj =  Instantiate(logo,transform.position,Quaternion.identity);
         obj.transform.parent = transform;
+
+        state = STATE.STATE_START;
 	}
-	
-	// Update is called once per frame
-	void Update ()
-    {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case STATE.STATE_START:
+                {
+                    alpha += 1f / 60;
+
+                    if (alpha >= 1f)
+                    {
+                        state = STATE.STATE_NORMAL;
+                    }
+                    break;
+                }
+            case STATE.STATE_NORMAL:
+                {
+                    break;
+                }
+            case STATE.STATE_END:
+                {
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+
+        obj.GetComponent<MeshRenderer>().material.SetFloat("_alpha", alpha);
+    }
 
     public void AllDelete()
     {
         Destroy(obj);
         Destroy(gameObject);
+    }
+
+    public void AlphaState()
+    {
+        switch (state)
+        {
+            case STATE.STATE_START:
+                {
+                    state = STATE.STATE_NORMAL;
+                    break;
+                }
+            case STATE.STATE_NORMAL:
+                {
+                    state = STATE.STATE_END;
+                    break;
+                }
+            case STATE.STATE_END:
+                {
+                    state = STATE.STATE_START;
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
     }
 }
