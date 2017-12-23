318513f6b32ce928000403a2f6de6a4bce1ab8d8
nakayamakazuma <gumi.green.m793s@gmail.com>
2017-12-24 03:42:56 +0900

    テックキッズを犠牲にしたミニオンAI超絶ぱわーあっぷ

--- MST13/Assets/MSTAsseats/Script/game/Bullet.cs
+++ MST13/Assets/MSTAsseats/Script/game/Bullet.cs
@@ -6,9 +6,10 @@ using TrueSync;
 public class Bullet : TrueSyncBehaviour {
 
     private minion target;
-    private float speed;
     private int shootOwner;
+    private int damage;
 
+    [SerializeField, TooltipAttribute("速度")] private FP speed;
     [SerializeField, TooltipAttribute("消える範囲")] private float range;
 
 	// Use this for initialization
@@ -22,17 +23,21 @@ public class Bullet : TrueSyncBehaviour {
 	}
 
     public override void OnSyncedUpdate(){
-        transform.LookAt(target.transform.position);
-        transform.Translate(transform.forward * speed);
+        TSVector vec = target.tsTransform.position - tsTransform.position;
+        vec = TSVector.Normalize(vec);
+        tsTransform.Translate(vec * speed, Space.World);
 
-        if(Vector3.Distance(transform.position, target.transform.position) < range){
-            TrueSyncManager.SyncedDestroy(gameObject);
+        if (TSMath.Abs(TSVector.Distance(tsTransform.position, target.tsTransform.position)) < range)
+        {
+            Destroy(gameObject);
+            target.AddDamage(damage);
         }
     }
 
-    public void Create(minion target, int shootOwner){
+    public void Create(minion target, int shootOwner, int damage){
         this.target = target;
         this.shootOwner = shootOwner;
+        this.damage = damage;
     }
 
     public int GetOwner(){
