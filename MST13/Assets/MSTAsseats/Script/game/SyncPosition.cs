f7186408aabdc3cf3d718e50bd3ef01750da64b5
nakayamakazuma <gumi.green.m793s@gmail.com>
2018-01-12 03:53:51 +0900

    no message

---
+++ MST13/Assets/MSTAsseats/Script/game/SyncPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TrueSync;
+
+public class SyncPosition : TrueSyncBehaviour {
+
+    private GameObject imageTarget;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public override void OnSyncedStart(){
+        imageTarget = GameObject.Find("ImageTarget");
+    }
+
+    public override void OnSyncedUpdate(){
+        FP posY = imageTarget.GetComponent<TSTransform>().position.y;
+        tsTransform.position = new TSVector(tsTransform.position.x, posY, tsTransform.position.z);
+    }
+}
